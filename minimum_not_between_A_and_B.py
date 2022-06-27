a6e4f04b0ff3bb73d83deddd98294f35cbcfc39a
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-27 23:51:56 +0530

    n=int(input()) a=list(map(int,input().split())) b,c=map(int,input().split()) co=0 x=[] for i in a:     if(i<b or i>c and i not in x):         co+=1         x+=[i] if(co==0):     print('-1')
    else:
        print(min(x))

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+b,c=map(int,input().split())
+co=0
+x=[]
+for i in a:
+    if(i<b or i>c and i not in x):
+        co+=1
+        x+=[i]
+if(co==0):
+    print('-1')
+
+else:
+    print(min(x))
