e2094832899c67738d4f9b19cda285dfe2c4b91c
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-28 15:50:34 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] c=0 for i in a:     i=str(i)     s=len(i)     b+=[s] for j in a:     j=str(j)     p=len(j)     if (p==min(b)):         c+=1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=0
+for i in a:
+    i=str(i)
+    s=len(i)
+    b+=[s]
+for j in a:
+    j=str(j)
+    p=len(j)
+    if (p==min(b)):
+        c+=1
+print(c)
