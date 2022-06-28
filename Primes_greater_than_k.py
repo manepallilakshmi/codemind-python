8e027531b1b2f6682f40e7f475a6a78faeb3efd8
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-28 14:59:22 +0530

    n=int(input()) a=list(map(int,input().split())) k=int(input()) c=0 for i in a:     if i>=k:         for j in range(2,i//2+1):             if i%j==0:                 break         else:             c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in a:
+    if i>=k:
+        for j in range(2,i//2+1):
+            if i%j==0:
+                break
+        else:
+            c+=1
+print(c)
+        
