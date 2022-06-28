34dde6cfe9623b904d3380a66a83e61ecff183ed
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-28 14:49:44 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 c=0 for i in a:     if i!=1:         for j in range(2,i//2+1):             if i%j==0:                 break         else:             s=s+i             c+=1 print("%0.2f"%(s/c))

---
+++ Average_of_primes.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+c=0
+for i in a:
+    if i!=1:
+        for j in range(2,i//2+1):
+            if i%j==0:
+                break
+        else:
+            s=s+i
+            c+=1
+print("%0.2f"%(s/c))
+        
