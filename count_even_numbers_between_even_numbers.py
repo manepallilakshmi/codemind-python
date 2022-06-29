27513860e3d9ce64f836348d26be28cf100cfa29
manepallilakshmi <97447403+manepallilakshmi@users.noreply.github.com>
2022-06-29 21:44:59 +0530

    n=int(input()) a=list(map(int,input().split())) co=0 for i in range(0,n):     if a[i]%2==0:         c=i         break for j in range(n-1,-1,-1):     if a[j]%2==0:         d=j         break for k in range(c+1,d):     if a[k]%2==0:         co+=1 print(co)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+co=0
+for i in range(0,n):
+    if a[i]%2==0:
+        c=i
+        break
+for j in range(n-1,-1,-1):
+    if a[j]%2==0:
+        d=j
+        break
+for k in range(c+1,d):
+    if a[k]%2==0:
+        co+=1
+print(co)
